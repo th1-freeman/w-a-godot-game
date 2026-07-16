94e40525368f7d9b7eaa4460ff3e6b5257cef96c
Arindam <arindam1775@gmail.com>
2026-07-16 21:44:10 +0530

    Improved viewcone functionality. Lose condition pending.

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 @export var Angular_Frequency = 0.025
-
+@onready var Sprite = get_node("Tile0085")
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("RIGHT", "LEFT"), Input.get_axis("UP", "DOWN"))
 	if direction:
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2.ZERO
 		
 	if velocity:
-		rotation_degrees = 10*sin(Angular_Frequency*Time.get_ticks_msec())
+		Sprite.rotation_degrees = 10*sin(Angular_Frequency*Time.get_ticks_msec())
 	else:
-		rotation_degrees = 0
+		Sprite.rotation_degrees = 0
 	move_and_slide()

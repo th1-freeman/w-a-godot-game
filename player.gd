2ba4f19857beb5818ac2b043f171ea4a95856d02
Arindam <arindam1775@gmail.com>
2026-07-16 17:45:23 +0530

    Improved player movement. Tileset added

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+@export var Angular_Frequency = 0.025
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("RIGHT", "LEFT"), Input.get_axis("UP", "DOWN"))
@@ -8,5 +9,9 @@ func _physics_process(delta: float) -> void:
 		velocity = direction*SPEED
 	else:
 		velocity = Vector2.ZERO
-
+		
+	if velocity:
+		rotation_degrees = 10*sin(Angular_Frequency*Time.get_ticks_msec())
+	else:
+		rotation_degrees = 0
 	move_and_slide()

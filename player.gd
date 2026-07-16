34fd2b98d76fa0e7874f203979b3a24d3864a34e
Arindam <arindam1775@gmail.com>
2026-07-16 17:17:32 +0530

    Basic Player Movement

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Vector2(Input.get_axis("RIGHT", "LEFT"), Input.get_axis("UP", "DOWN"))
+	if direction:
+		velocity = direction*SPEED
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()

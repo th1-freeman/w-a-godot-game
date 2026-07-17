2d0e688fc83527b2286474bc83391aefb62c0fa7
Arindam <arindam1775@gmail.com>
2026-07-17 12:51:38 +0530

    Added mouse tracking to viewcone

--- SCRIPTS/player.gd
+++ SCRIPTS/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 @export var Angular_Frequency = 0.025
 @onready var Sprite = get_node("Tile0085")
+@onready var Viewcone = get_node("Area2D")
+
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("RIGHT", "LEFT"), Input.get_axis("UP", "DOWN"))
 	if direction:
@@ -15,3 +17,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		Sprite.rotation_degrees = 0
 	move_and_slide()
+	
+	Viewcone.look_at(get_global_mouse_position())

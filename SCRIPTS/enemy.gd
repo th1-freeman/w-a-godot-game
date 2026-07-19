f16ff5fe65d60f21fc22d2cd7cc988492df1c954
Arindam <arindam1775@gmail.com>
2026-07-19 09:17:36 +0530

    Basic Game loop done. Gonna create v1.0 release now

--- SCRIPTS/enemy.gd
+++ SCRIPTS/enemy.gd
@@ -4,6 +4,7 @@ var can_move = true
 @onready var Player = get_tree().root.get_node("Node2D/Player")
 @export var SPEED = 10
 
+
 func _physics_process(delta: float) -> void:
 	if can_move:
 		velocity = Vector2(Player.position.x - position.x, Player.position.y - position.y) * 1/SPEED
